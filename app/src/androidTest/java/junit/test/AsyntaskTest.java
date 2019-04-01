456e555f3d6fb319e68c97c146486eccc70d8ed9
serged97 <pessokho@gmail.com>
2019-04-01 08:45:02 +0100

    fixed test classes issues

--- app/src/androidTest/java/junit/test/AsyntaskTest.java
+++ app/src/androidTest/java/junit/test/AsyntaskTest.java
@@ -1,16 +1,19 @@
 package junit.test;
 
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.udacity.gradle.builditbigger.MainActivity;
 import com.udacity.gradle.builditbigger.R;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.core.deps.guava.base.CharMatcher.isNot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
@@ -19,7 +22,8 @@ import static org.hamcrest.Matchers.not;
 @RunWith(AndroidJUnit4.class)
 public class AsyntaskTest{
 
-    private static String RESULT_TXT = "Hi, Serge Pessokho, Second Funny Jokes";
+	@Rule
+	public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void recip_list_test(){
